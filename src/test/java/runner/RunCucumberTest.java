056d558590d0c6a6bf1da1da82091458755dfcf1
Lê Phước Thành <thanhle.dev.93@gmail.com>
2021-07-22 19:08:05 +0700

    Update RunCucumberTest.java

--- src/test/java/runner/RunCucumberTest.java
+++ src/test/java/runner/RunCucumberTest.java
@@ -2,15 +2,12 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 
 @CucumberOptions(
         features = {"src/test/resources/features"},
@@ -23,14 +20,12 @@ import java.net.MalformedURLException;
 public class RunCucumberTest extends AbstractTestNGCucumberTests{
     @BeforeSuite
     public static void setup() throws IOException, InterruptedException {
-        System.out.println("@BeforeClass");
         Runtime.getRuntime().exec("cmd /c start " +System.getProperty("user.dir") + File.separator + ".github" + File.separator + "workflows" + File.separator + "start_dockergrid.bat");
-        Thread.sleep(25000);
+        Thread.sleep(20000);
     }
 
     @AfterSuite
     public static void tearDown() throws IOException, InterruptedException {
-        System.out.println("@AfterClass");
         Runtime.getRuntime().exec("cmd /c start " +System.getProperty("user.dir") + File.separator + ".github" + File.separator + "workflows" + File.separator + "stop_dockergrid.bat");
         Thread.sleep(15000);
     }
