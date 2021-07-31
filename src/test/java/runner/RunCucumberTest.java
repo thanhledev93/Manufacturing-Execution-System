95958f7e32d4995184e500f4a4b1906887c8059a
Lê Phước Thành <thanhle.dev.93@gmail.com>
2021-07-31 10:15:18 +0700

    Update RunCucumberTest.java

--- src/test/java/runner/RunCucumberTest.java
+++ src/test/java/runner/RunCucumberTest.java
@@ -21,6 +21,7 @@ public class RunCucumberTest extends AbstractTestNGCucumberTests{
     @BeforeSuite
     public static void setup() throws IOException, InterruptedException {
         System.out.println("setup() running");
+        System.out.println(System.getProperty("user.dir") + File.separator + ".github" + File.separator + "workflows");
         Runtime.getRuntime().exec("cmd /c start " +System.getProperty("user.dir") + File.separator + ".github" + File.separator + "workflows" + File.separator + "start_dockergrid.bat");
         Thread.sleep(60000);
     }
