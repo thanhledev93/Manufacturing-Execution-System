008d4fc88b506ece668295f61115cc13341374a1
Lê Phước Thành <thanhle.dev.93@gmail.com>
2021-08-02 14:01:27 +0700

    Update Hooks.java

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -26,7 +26,7 @@ public class Hooks extends BaseClass {
     }
     @AfterStep
     public void DelayAfterStep() throws InterruptedException {
-        Thread.sleep(500);
+        Thread.sleep(300);
     }
 
     @After
